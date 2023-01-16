6f98cda433e1c8fac32854ba90b9ec6a181ad43d
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-01-15 18:51:42 -0600

    Update Homescreen.js

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -3,17 +3,18 @@ import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 
+
 const Homescreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-        <Row>
-            {products.map(product => (
-                <Col sm={12} md={6} lg={4}>
-                    <Product product={product} />
-                </Col>
-            ))}
-        </Row>   
+      <Row>
+        {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>
+            </Col>
+        ))}
+      </Row>
     </>
   )
 }
