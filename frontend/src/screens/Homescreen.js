e6b0f960acf8c199c1453d92cbdc8f1fcceb0c54
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-01-15 19:22:24 -0600

    frontend update

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -6,16 +6,16 @@ import products from '../products'
 
 const Homescreen = () => {
   return (
-    <div>
+    <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-            <Col sm={12} md={6} lg={4} xl={3}>
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product}/>
             </Col>
         ))}
       </Row>
-    </div>
+    </>
   )
 }
 
