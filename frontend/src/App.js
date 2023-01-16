2d1e8c8b4a474cb13976c3caca4d120d4af2c9f2
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-01-15 18:55:11 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Container} from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Homescreen from "./screens/Homescreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
     <Header />
       <main className="py-3">
         <Container>
-          <Homescreen />
+          <HomeScreen />
        </Container>
      </main>
     <Footer />
