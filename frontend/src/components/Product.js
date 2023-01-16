b02e310392ab4a109894318aeb7806a66ee26821
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-01-15 18:21:56 -0600

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -8,6 +8,20 @@ const Product = ({product}) => {
         <Card.Img src={product.image} variant='top'>
         </Card.Img>
         </a> 
+
+        <Card.Body>
+          <a href={`/product/${product._id}`}>
+            <Card.Title as='div'><strong>{product.name}</strong>
+           </Card.Title>
+          </a> 
+
+          <Card.Text as='div'> 
+            <div className='my-3'>
+              {product.rating} from {product.numReviews} reviews
+            </div>
+          </Card.Text>
+          <Card.Text as='h3'>${product.price} </Card.Text>         
+        </Card.Body>
     </Card>
   )
 }
